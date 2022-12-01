6383cc1073d1f5453bec0e9630a4801025ca29a1
Mohammad Rowshan <37102311+mohammad-rowshan@users.noreply.github.com>
2022-12-01 12:39:38 +1100

    Update dispersion-bound.m

--- dispersion-bound.m
+++ dispersion-bound.m
@@ -1,3 +1,15 @@
+# Dispersion Bound for Finite Blocklength Codes over BI-AWGN Channel ###################
+#
+# Copyright (c) 2022, Mohammad Rowshan
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification, 
+# are permitted provided that:
+# the source code retains the above copyright notice, and te redistribtuion condition.
+# 
+# Freely distributed for educational and research purposes
+########################################################################################
+
 N=2^7;  % block length
 R=0.5;  % code rate
 EbN0dB=1:0.1:4;  % the target range of SNR per info bit (Eb/N0) in dB
@@ -10,7 +22,6 @@ for rho = SNR
     C = integral(f, Zmin, Zmax);    % channel capacity
     g = @(z) exp(-z.^2/2)/sqrt(2*pi) .* (log2(2) - log2(1+exp(-2*rho-2*sqrt(rho)*z))-C).^2;
     V = integral(g, Zmin, Zmax);    % channel dispercion
-    %V=(rho/2).*((rho+2)./(rho+1).^2)*(log2(exp(1))^2);     % alternative
     epsilonNA(end+1)=qfunc((C-R+0.5/N*log2(N))./(sqrt(V/N))); 
 end
 figure
